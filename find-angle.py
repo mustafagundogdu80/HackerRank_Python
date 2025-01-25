28df664fd41bfaddc3494ce1cec3e3ae6bbafb2a
mustafagundogdu80 <mustafagundogdu@hotmail.com>
2025-01-25 03:24:55 +0100

    Create find-angle.py

---
+++ find-angle.py
@@ -0,0 +1,14 @@
+import math
+import sys
+
+# data = sys.stdin.read()
+# lines = data.splitlines()
+# ab = int(input())
+# bc = int(input())
+# theta_radian = math.atan(ab/bc)
+# theta_degree = math.degrees(theta_radian)
+# angle = round(theta_degree)
+
+# print(f"{angle}°")
+for i in range(70, 500):
+    print(i,"-",chr(i))
